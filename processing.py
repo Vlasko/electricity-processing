fdacab248ae6d0734561dd5a33d600586a2933d1
Vlasko <luka.j.v@gmail.com>
2019-12-18 15:07:12 +0000

    added functionality

--- processing.py
+++ processing.py
@@ -1,13 +1,20 @@
-# Device Manager
+# Standard Library Imports
 import os
+from statistics import mean, stdev
 
+# Third Party Imports
+
+# Local Application Imports
 from functions.AWS_functions import download_from_aws, get_bucket_contents
+from functions.graphing import simple_graphing, total_graphing, cropped_graphing
+# %% plot inline figure
+from functions.cleaning_functions import clean_demand, clean_price
 
 buckets = {'Prices':'lukaprices',
            'Demand':'fridgedemand',
            'Temperature':'fridgetemperature'}
 
-directory = os.path.split(os.getcwd())[0]
+directory = os.getcwd()
 for folder, bucket_name in buckets.items():
     bucket_contents = get_bucket_contents(bucket_name)
     file_list = os.listdir(directory+'files/'+folder+'/')
@@ -17,3 +24,16 @@ for folder, bucket_name in buckets.items():
                               directory+'files/'+folder+'/'+aws_filename)
         else:
             pass
+
+directory = os.getcwd()
+
+demand_path = directory+'/files/demand/'
+demand_df = clean_demand(demand_path)
+
+price_path = directory+'/files/prices/'
+price_df = clean_price(price_path)
+
+# %% display a dataframe
+price_df
+
+simple_graphing(price_df)
