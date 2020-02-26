727ad238c56f49825f27969ecbdbd1ef3e3f1f8e
Vlasko <luka.j.v@gmail.com>
2020-02-26 17:29:03 +0000

    Added price stats

--- graphing.py
+++ graphing.py
@@ -14,6 +14,9 @@ demand_df = clean_demand(demand_path)
 price_path = directory+'/files/prices/'
 price_df = clean_price(price_path)
 
+combined_df = pd.concat([demand_df, price_df], axis=1).dropna()
+combined_df.head()
+combined_df.loc[:,'Price (p)'] = combined_df.loc[:,'Demand (W)']*(0.25/1000)*combined_df.loc[:,'Electricity Price(p/kWh)']
 # %% display a dataframe
 merged_df = pd.merge(demand_df, price_df, left_index = True, right_index = True)
 
