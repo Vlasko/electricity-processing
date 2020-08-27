fdb04a474c835747595fdd33995fc1c40df4554b
Vlasko <luka.j.v@gmail.com>
2020-08-27 10:19:46 +0100

    save combined data into cleaned data folder

--- graphing.py
+++ graphing.py
@@ -20,6 +20,11 @@ combined_df.loc[:,'Price (p)'] = combined_df.loc[:,'Demand (W)']*(0.25/1000)*com
 combined_df.describe()
 combined_df.loc[:,'Price (p)'].sum()
 
+data_start = str(combined_df.index[0].date())
+data_end = str(combined_df.index[-1].date())
+
+combined_df.to_csv('/Users/gianluca/Documents/Projects/electricity-processing/files/cleaned_data/'+data_start+'_'+data_end+'.csv')
+
 combined_graphing(price_df['2020-01-01':],demand_df['2020-01-01':])
 simple_graphing(price_df['2020-01-01':])
 simple_graphing(demand_df['2020-01-01':])
